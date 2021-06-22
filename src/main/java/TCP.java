0dde578d1035f98cbbc073e3e41da7c517ffe714
Luis Alejandro Mendoza Franco <lalexmf117@gmail.com>
2021-06-21 19:57:36 -0500

    TCP completo de Jose

--- src/main/java/TCP.java
+++ src/main/java/TCP.java
@@ -11,6 +11,7 @@ public class TCP {
     private byte [] acknowledgmentnumber; //# De byte que se espera recibir en el siguiente segmento
     private byte [] hlen; // Logitud de la cabecera media.
     private byte [] banderas;
+    private int [] bauxiliar; // Auxiliar para banderas.
     private byte [] windowssize; // Espacio en el buffer de datos de TCP
     private byte [] checksum;
     private byte [] urgentpointer; // Nos indica donde terminan los datos urgentes
@@ -21,11 +22,11 @@ public class TCP {
         destinationport = new byte [2];
         sequencenumber = new byte [4];
         acknowledgmentnumber = new byte [4];
-        hlen = new byte[1];
         banderas = new byte[1];
+        bauxiliar = new int[8];
         windowssize = new byte [2];
         checksum = new byte[2];
-        urgentpointer = new byte[1];
+        urgentpointer = new byte[2];
     }
     
     public void analisisTCP(byte[] trama)
@@ -34,7 +35,6 @@ public class TCP {
         this.getDestinationPort (trama);
         this.getSequenceNumber (trama);
         this.getAcknowledgmentNumber (trama);
-        this.getHlen(trama);
         this.getBanderas(trama);
         this.getWindowsSize (trama);
         this.getChecksum (trama);
@@ -72,15 +72,16 @@ public class TCP {
         }
     }
     
-    private void getHlen(byte[] trama)
-    {
-            hlen [0] = trama[12];
-    }
-    
     private void getBanderas(byte[] trama)
     {
-       //En proceso
-       
+        int convert;
+        /*banderas [0] */ convert= trama [13];
+        //convert = banderas [0] >> 2; 
+        for(int i=0; i<8; i++)
+        {
+            bauxiliar[i] = convert % 2;
+            convert = convert / 2;
+        }
     }
     
     private void getWindowsSize(byte[] trama)
@@ -101,18 +102,23 @@ public class TCP {
     
     private void getUrgentPointer(byte[] trama)
     {
-            urgentpointer [0] = trama[19];
+            urgentpointer [0] = trama[18];
+            urgentpointer [1] = trama[19];
     }
     
     private String tostrSourcePort()
     {
-        String sPort = "\tSource Port: " + ByteArrays.toHexString(sourceport, "-") + "\n";
+        int ch = sourceport[1] & 255 ;
+        ch += ((sourceport[0]& 255)*256);
+        String sPort = "\tSource Port: " + ch/*ByteArrays.toHexString(sourceport, "-")*/ + "\n";
         return sPort;
     }
     
     private String tostrDestinationPort()
     {
-        String dPort = "\tDestination Port: " + ByteArrays.toHexString(destinationport, "-") + "\n";
+        int ch = destinationport[1] & 255 ;
+        ch += ((destinationport[0]& 255)*256);
+        String dPort = "\tDestination Port: " + ch/*ByteArrays.toHexString(destinationport, "-") */+ "\n";
         return dPort;
     }
     
@@ -130,19 +136,26 @@ public class TCP {
     
     private String tostrBanderas()
     {
-        String BanDer = "\tBanderas: \n" ;
+        String BanDer = "\t1 = Bandera activa   y   0 = Bandera inactiva. \n\tBanderas: "
+                + "\n\tCWR: " + bauxiliar[7] + "\n\tECN: " + bauxiliar[6] + "\n\tUrg: " + bauxiliar[5] 
+                + "\n\tACK: " + bauxiliar[4] + "\n\tPSH: " + bauxiliar[3] + "\n\tRST: " +bauxiliar[2]
+                + "\n\tSYN: " + bauxiliar[1] + "\n\tFIN: " + bauxiliar[0] + "\n";
         return BanDer;
     }
     
     private String tostrWindowsSize()
     {
-        String wSize = "\tWindows Size: " + ByteArrays.toHexString(windowssize, "-") + "\n";
+        int ch = windowssize[1] & 255 ;
+        ch += ((windowssize[0]& 255)*256);
+        String wSize = "\tWindows Size: " +ch/*ByteArrays.toHexString(windowssize, "-") */+ "\n";
         return wSize;
     }
     
     private String tostrChecksum()
     {
-        String cSum = "\tChecksum: " + ByteArrays.toHexString(checksum, "-") + "\n";
+        int ch = checksum[1] & 255 ;
+        ch += ((checksum[0]& 255)*256);
+        String cSum = "\tChecksum: " + ch/*ByteArrays.toHexString(checksum, "-")*/ + "\n";
         return cSum;
     }
     
